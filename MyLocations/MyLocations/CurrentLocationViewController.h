0ba184e0666ed9c5dbc0704672d8b208ec747e09
shiziwen <shiziwen@gmail.com>
2016-01-17 00:17:10 +0800

    added codes for 3-chapter6

--- MyLocations/MyLocations/CurrentLocationViewController.h
+++ MyLocations/MyLocations/CurrentLocationViewController.h
@@ -11,10 +11,11 @@
 @interface CurrentLocationViewController : UIViewController
 @property (nonatomic, weak) IBOutlet UILabel *messageLabel;
 @property (nonatomic, weak) IBOutlet UILabel *latitudeLabel;
-@property (nonatomic, weak) IBOutlet UILabel *longtitudeLabel;
+@property (nonatomic, weak) IBOutlet UILabel *longitudeLabel;
 @property (nonatomic, weak) IBOutlet UILabel *addressLabel;
 @property (nonatomic, weak) IBOutlet UIButton *tagButton;
 @property (nonatomic, weak) IBOutlet UIButton *getButton;
 
+- (IBAction)getLocation:(id)sender;
 @end
 
--- MyLocations/MyLocations/CurrentLocationViewController.m
+++ MyLocations/MyLocations/CurrentLocationViewController.m
@@ -24,4 +24,8 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)getLocation:(id)sender {
+    
+}
+
 @end
