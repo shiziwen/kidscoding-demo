3ec1d6ef887ac3babe46b22f66aa556c3d849b0f
shiziwen <shiziwen@gmail.com>
2016-01-17 03:36:54 +0800

    codes for l3-chapter 8: added message for location result

--- MyLocations/MyLocations/CurrentLocationViewController.m
+++ MyLocations/MyLocations/CurrentLocationViewController.m
@@ -14,6 +14,9 @@
 
 @implementation CurrentLocationViewController {
     CLLocationManager *_locationManager;
+    CLLocation *_location;
+    BOOL _updatingLocation;
+    NSError *_lastLocationError;
 }
 
 - (id)initWithCoder:(NSCoder *)aDecoder {
@@ -26,6 +29,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self updateLabels];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -36,20 +40,85 @@
 - (IBAction)getLocation:(id)sender {
     NSLog(@"touch getLocation");
     
+    [self startLocaionManager];
+    [self updateLabels];
+}
+
+- (void)startLocaionManager {
     _locationManager.delegate = self;
     _locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters;
     [_locationManager requestWhenInUseAuthorization];
-//    [_locationManager requestAlwaysAuthorization];
+    //    [_locationManager requestAlwaysAuthorization];
     [_locationManager startUpdatingLocation];
+    
+    _updatingLocation = YES;
+}
+
+- (void)stopLocationManager {
+    if (_updatingLocation) {
+        [_locationManager stopUpdatingLocation];
+        _locationManager.delegate = nil;
+        _updatingLocation = NO;
+    }
 }
 
 # pragma mark CLLocationManagerDelegate
 - (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error {
     NSLog(@"get location error: %@", error);
+    
+    if (error.code == kCLErrorLocationUnknown) {
+        return;
+    }
+    
+    [self stopLocationManager];
+    _lastLocationError = error;
+    _location = nil;
+    
+    [self updateLabels];
 }
 
 - (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray<CLLocation *> *)locations {
     CLLocation *newLocation = [locations lastObject];
     NSLog(@"new Points: %@", newLocation);
+    
+    _lastLocationError = nil;
+    _location = newLocation;
+    [self updateLabels];
 }
+
+- (void)updateLabels {
+    NSLog(@"updates");
+    if (_location != nil) {
+        self.latitudeLabel.text = [NSString stringWithFormat:@"%.8f", _location.coordinate.latitude];
+        self.longitudeLabel.text = [NSString stringWithFormat:@"%.8f", _location.coordinate.latitude];
+        self.tagButton.hidden = NO;
+        self.messageLabel.text = @"";
+    } else {
+        self.latitudeLabel.text = @"";
+        self.longitudeLabel.text = @"";
+        self.addressLabel.text = @"";
+        self.tagButton.hidden = YES;
+        self.messageLabel.text = @"Press the Button to Start";
+        
+        NSString *statusMessage;
+        if (_lastLocationError != nil) {
+            if ([_lastLocationError.domain isEqualToString:kCLErrorDomain] &&
+                _lastLocationError.code == kCLErrorDenied) {
+                statusMessage = @"Sorry, user has denied the location service";
+//                [_locationManager requestAlwaysAuthorization];
+            } else {
+                statusMessage = @"Sorry, get location error";
+            }
+        } else if (![CLLocationManager locationServicesEnabled]) {
+            statusMessage = @"Sorry, user has denied the location service";
+        } else if (_updatingLocation) {
+            statusMessage = @"Searching location...";
+        }
+        self.messageLabel.text = statusMessage;
+    }
+    
+    
+}
+
+
 @end
