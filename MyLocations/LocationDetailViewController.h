95f15d4aa4d2d889f8caca7010739dda6fb37842
shiziwen <shiziwen@gmail.com>
2016-01-19 14:45:53 +0800

    added codes for lesson3-chapter15 added tableview controller for tag location

---
+++ MyLocations/LocationDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  LocationDetailViewController.h
+//  MyLocations
+//
+//  Created by mac on 16/1/19.
+//  Copyright © 2016年 shiziwen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LocationDetailViewController : UITableViewController
+
+@end
