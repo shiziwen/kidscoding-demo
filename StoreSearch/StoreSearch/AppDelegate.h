222e6f1e12aab945ad149eb5f5ee61078594d85c
shiziwen <shiziwen@gmail.com>
2016-01-25 00:52:57 +0800

    init storesearch Removed the storyboard and replaced it with a nib

---
+++ StoreSearch/StoreSearch/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.h
+//  StoreSearch
+//
+//  Created by mac on 16/1/25.
+//  Copyright © 2016年 shiziwen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@class SearchViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+@property (strong, nonatomic) SearchViewController *searchViewController;
+
+@end
+
---
+++ StoreSearch/StoreSearch/SearchViewController.h
@@ -0,0 +1,13 @@
+//
+//  SearchViewController.h
+//  StoreSearch
+//
+//  Created by mac on 16/1/25.
+//  Copyright © 2016年 shiziwen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchViewController : UIViewController
+
+@end
