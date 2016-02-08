235460e80f122a9a79d9aa71a28f01264c5f14c6
shiziwen <shiziwen@gmail.com>
2016-02-09 01:32:19 +0800

    doing fake searches

--- StoreSearch/StoreSearch/SearchViewController.m
+++ StoreSearch/StoreSearch/SearchViewController.m
@@ -8,15 +8,28 @@
 
 #import "SearchViewController.h"
 
-@interface SearchViewController ()
-
+@interface SearchViewController () <UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate>
+@property (nonatomic, weak) IBOutlet UISearchBar *searchBar;
+@property (nonatomic, weak) IBOutlet UITableView *tableView;
 @end
 
-@implementation SearchViewController
+@implementation SearchViewController {
+    NSMutableArray *_searchResults;
+}
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    self.tableView.contentInset = UIEdgeInsetsMake(64, 0, 0, 0);
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,14 +37,48 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
+# pragma mark - UITableViewDataSource
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    if (_searchResults == nil) {
+        return 0;
+    } else {
+        return [_searchResults count];
+    }
 }
-*/
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *CellIdentifier = @"SearchResultCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+    }
+    
+    cell.textLabel.text = _searchResults[indexPath.row];
+    return cell;
+}
+
+#pragma mark - UISearchBarDelegate
+
+- (void)searchBarSearchButtonClicked:(UISearchBar *)searchBar {
+    // the keyboard will hide itself until you tap inside the search bar again
+    [searchBar resignFirstResponder];
+    
+    NSLog(@"The search text is %@", searchBar.text);
+    
+    _searchResults = [NSMutableArray arrayWithCapacity:10];
+    
+    for (int i = 0; i < 3; i++) {
+        [_searchResults addObject:[NSString stringWithFormat:@"Fake result %d for %@", i, searchBar.text]];
+    }
+    
+    [self.tableView reloadData];
+}
+
+- (UIBarPosition)positionForBar:(id <UIBarPositioning>)bar
+{
+    return UIBarPositionTopAttached;
+}
 
 @end
