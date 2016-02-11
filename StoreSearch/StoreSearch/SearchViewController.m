6b92427117efa3089c7daded422c038a8975c352
shiziwen <shiziwen@gmail.com>
2016-02-12 01:06:59 +0800

    A better SearchResult

--- StoreSearch/StoreSearch/SearchViewController.m
+++ StoreSearch/StoreSearch/SearchViewController.m
@@ -53,6 +53,32 @@ static NSString * const NothingFoundCellIdentifier = @"NothingFoundCell";
 
 # pragma mark - UITableViewDataSource
 
+- (NSString *)kindForDisplay:(NSString *)kind {
+    if ([kind isEqualToString:@"album"]) {
+        return @"Album";
+    } else if ([kind isEqualToString:@"audiobook"]) {
+        return @"Audio Book";
+    } else if ([kind isEqualToString:@"book"]) {
+        return @"Book";
+    } else if ([kind isEqualToString:@"ebook"]) {
+        return @"E-Book";
+    } else if ([kind isEqualToString:@"feature-movie"]) {
+        return @"Movie";
+    } else if ([kind isEqualToString:@"music-video"]) {
+        return @"Music Video";
+    } else if ([kind isEqualToString:@"podcast"]) {
+        return @"Podcast";
+    } else if ([kind isEqualToString:@"software"]) {
+        return @"App";
+    } else if ([kind isEqualToString:@"song"]) {
+        return @"Song";
+    } else if ([kind isEqualToString:@"tv-episode"]) {
+        return @"TV Episode";
+    } else {
+        return kind;
+    }
+}
+
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     if (_searchResults == nil) {
         return 0;
@@ -78,7 +104,14 @@ static NSString * const NothingFoundCellIdentifier = @"NothingFoundCell";
         
         SearchResult *searchResult = _searchResults[indexPath.row];
         cell.nameLabel.text = searchResult.name;
-        cell.artistNameLabel.text = searchResult.artistName;
+        
+        NSString *artistName = searchResult.artistName;
+        if (artistName == nil) {
+            artistName = @"Unknown";
+        }
+        
+        NSString *kind = [self kindForDisplay:searchResult.kind];
+        cell.artistNameLabel.text = [NSString stringWithFormat:@"%@ (%@)", artistName, kind];
         
         return cell;
 
@@ -147,8 +180,82 @@ static NSString * const NothingFoundCellIdentifier = @"NothingFoundCell";
     }
     
     for (NSDictionary *resultDict in array) {
-        NSLog(@"wrapperType: %@, kind: %@", resultDict[@"wrapperType"], resultDict[@"kind"]);
+//        NSLog(@"wrapperType: %@, kind: %@", resultDict[@"wrapperType"], resultDict[@"kind"]);
+        SearchResult *searchResult;
+        NSString *wrapperType = resultDict[@"wrapperType"];
+        NSString *kind = resultDict[@"kind"];
+        
+        if ([wrapperType isEqualToString:@"track"]) {
+            searchResult = [self parseTrack:resultDict];
+        } else if ([wrapperType isEqualToString:@"audiobook"]) {
+            searchResult = [self parseAudioBook:resultDict];
+        } else if ([wrapperType isEqualToString:@"software"]) {
+            searchResult = [self parseSoftware:resultDict];
+        } else if ([kind isEqualToString:@"ebook"]) {
+            searchResult = [self parseEBook:resultDict];
+        }
+        
+        if (searchResult != nil) {
+            [_searchResults addObject:searchResult];
+        }
     }
+}
+
+- (SearchResult *)parseTrack:(NSDictionary *)dictionary {
+    SearchResult *searchResult = [[SearchResult alloc] init];
+    searchResult.name = dictionary[@"trackName"];
+    searchResult.artistName = dictionary[@"artistName"];
+    searchResult.artworkURL60 = dictionary[@"artworkURL60"];
+    searchResult.artworkURL100 = dictionary[@"artworkURL100"];
+    searchResult.storeURL = dictionary[@"trackViewUrl"];
+    searchResult.kind = dictionary[@"kind"];
+    searchResult.price = dictionary[@"trackPrice"];
+    searchResult.currency = dictionary[@"currency"];
+    searchResult.genre = dictionary[@"primaryGenreName"];
+    
+    return searchResult;
+}
+
+- (SearchResult *)parseAudioBook:(NSDictionary *)dictionary {
+    SearchResult *searchResult = [[SearchResult alloc] init];
+    searchResult.name = dictionary[@"collectionName"];
+    searchResult.artistName = dictionary[@"artistName"];
+    searchResult.artworkURL60 = dictionary[@"artworkUrl60"];
+    searchResult.artworkURL100 = dictionary[@"artworkUrl100"];
+    searchResult.storeURL = dictionary[@"collectionViewUrl"];
+    searchResult.kind = @"audiobook";
+    searchResult.price = dictionary[@"collectionPrice"];
+    searchResult.currency = dictionary[@"currency"];
+    searchResult.genre = dictionary[@"primaryGenreName"];
+    return searchResult;
+}
+
+- (SearchResult *)parseSoftware:(NSDictionary *)dictionary {
+    SearchResult *searchResult = [[SearchResult alloc] init];
+    searchResult.name = dictionary[@"trackName"];
+    searchResult.artistName = dictionary[@"artistName"];
+    searchResult.artworkURL60 = dictionary[@"artworkUrl60"];
+    searchResult.artworkURL100 = dictionary[@"artworkUrl100"];
+    searchResult.storeURL = dictionary[@"trackViewUrl"];
+    searchResult.kind = dictionary[@"kind"];
+    searchResult.price = dictionary[@"price"];
+    searchResult.currency = dictionary[@"currency"];
+    searchResult.genre = dictionary[@"primaryGenreName"];
+    return searchResult;
+}
+
+- (SearchResult *)parseEBook:(NSDictionary *)dictionary {
+    SearchResult *searchResult = [[SearchResult alloc] init];
+    searchResult.name = dictionary[@"trackName"];
+    searchResult.artistName = dictionary[@"artistName"];
+    searchResult.artworkURL60 = dictionary[@"artworkUrl60"];
+    searchResult.artworkURL100 = dictionary[@"artworkUrl100"];
+    searchResult.storeURL = dictionary[@"trackViewUrl"];
+    searchResult.kind = dictionary[@"kind"];
+    searchResult.price = dictionary[@"price"];
+    searchResult.currency = dictionary[@"currency"];
+    searchResult.genre = [(NSArray *)dictionary[@"genres"] componentsJoinedByString:@", "];
+    return searchResult;
 }
 
 - (void)searchBarSearchButtonClicked:(UISearchBar *)searchBar {
@@ -175,6 +282,7 @@ static NSString * const NothingFoundCellIdentifier = @"NothingFoundCell";
         NSLog(@"Dictionary: %@", dictionary);
         
         [self parseDictionary:dictionary];
+        [_searchResults sortUsingSelector:@selector(compareName:)];
 
         
         [self.tableView reloadData];
