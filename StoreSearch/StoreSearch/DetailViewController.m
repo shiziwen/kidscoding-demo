63b8d5f20cae1c01d2c129f1e78098f897d4b66c
shiziwen <shiziwen@gmail.com>
2016-02-16 23:10:17 +0800

    added poped up control

--- StoreSearch/StoreSearch/DetailViewController.m
+++ StoreSearch/StoreSearch/DetailViewController.m
@@ -7,8 +7,16 @@
 //
 
 #import "DetailViewController.h"
+#import <QuartzCore/QuartzCore.h>
 
-@interface DetailViewController ()
+@interface DetailViewController () <UIGestureRecognizerDelegate>
+@property (weak, nonatomic) IBOutlet UIView *popedView;
+@property (weak, nonatomic) IBOutlet UIImageView *artworkImageView;
+@property (weak, nonatomic) IBOutlet UILabel *nameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *artistNameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *kindLabel;
+@property (weak, nonatomic) IBOutlet UILabel *genreLabel;
+@property (weak, nonatomic) IBOutlet UIButton *priceButton;
 
 @end
 
@@ -17,6 +25,18 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    
+    UIImage *image = [[UIImage imageNamed:@"PriceButton"] resizableImageWithCapInsets:UIEdgeInsetsMake(0, 5, 0, 5)];
+    image = [image imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
+    [self.priceButton setBackgroundImage:image forState:UIControlStateNormal];
+    self.view.tintColor = [UIColor colorWithRed:20/255.0f green:160/255.0f blue:160/255.0f alpha:1.0f];
+    
+    self.popedView.layer.cornerRadius = 10.0f;
+    
+    UITapGestureRecognizer *gestureRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(close:)];
+    gestureRecognizer.cancelsTouchesInView = NO;
+    gestureRecognizer.delegate = self;
+    [self.view addGestureRecognizer:gestureRecognizer];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -43,5 +63,9 @@
 
 - (void)dealloc {
     NSLog(@"dealloc %@", self);
+}
+
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldReceiveTouch:(UITouch *)touch {
+    return (touch.view == self.view);
 }
 @end
